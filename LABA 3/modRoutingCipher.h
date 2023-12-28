3ce6ba7f8e04df9a1eba7e15d35889ecbe4dd4ae
zaharkuzmin <125679616+zaharkuzmin@users.noreply.github.com>
2023-12-28 23:48:50 +0300

    Create modRoutingCipher.h

---
+++ LABA 3/modRoutingCipher.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+class PerestanCipher
+{
+private:
+    int k;
+    wstring getValidOpenText(const std::wstring & s);
+    wstring getValidCipherText(const std::wstring & s);
+    int getValidKey(const int k, const std::wstring & s);
+public:
+    PerestanCipher()=delete;
+    PerestanCipher(int w);
+    wstring CoderPerestanCipher(PerestanCipher w, wstring& s);
+    wstring DecoderPerestanCipher(PerestanCipher w, wstring& s);
+};
+class cipher_error: public std::invalid_argument
+{
+public:
+    explicit cipher_error (const std::string& what_arg):
+        std::invalid_argument(what_arg) {}
+    explicit cipher_error (const char* what_arg):
+        std::invalid_argument(what_arg) {}
+};
