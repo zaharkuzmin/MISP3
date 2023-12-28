07673ed8008d5987d49593459d47f011358950db
zaharkuzmin <125679616+zaharkuzmin@users.noreply.github.com>
2023-12-28 23:47:08 +0300

    Create main.cpp

---
+++ LABA 3/main.cpp
@@ -0,0 +1,91 @@
+#include <UnitTest++/UnitTest++.h>
+#include <iostream>
+#include "modRoutingCipher.h"
+#include <locale>
+#include <codecvt>
+
+
+using namespace std;
+string wst (int k, wstring s1)
+{
+    PerestanCipher w(k);
+    wstring s=w.CoderPerestanCipher(w, s1);
+    const string s2 (s.begin(), s.end() );
+    return s2;
+}
+string wst1 (int k, wstring s1)
+{
+    PerestanCipher w(k);
+    wstring s=w.DecoderPerestanCipher(k, s1);
+    const string s2 (s.begin(), s.end() );
+    return s2;
+}
+SUITE (KeyTest)
+{
+    wstring test = L"PROGRAMMIROVANIE";
+    int k;
+    TEST (ValidKey) {
+        CHECK_EQUAL(wst(k=4,test),"PRIARARNOMOIGMVE");
+    }
+    TEST(EmptyKey) {
+        CHECK_THROW(wst(k=55,test), cipher_error);
+    }
+    TEST(NegativeKey) {
+        CHECK_THROW(wst(k=0,test), cipher_error);
+    }
+
+    TEST(A_characters_in_the_key_instead_of_numbers) {
+        CHECK_THROW(wst(k=-6,test), cipher_error);
+    }
+}
+SUITE(EncryptTest)
+{
+    TEST(ValidText) {
+        CHECK_EQUAL(wst(4,L"PROGRAMMIROVANIE"),"PRIARARNOMOIGMVE");
+    }
+    TEST(LowText) {
+        CHECK_EQUAL(wst(4,L"PRograMmiroVANie"),"PRIARARNOMOIGMVE");
+    }
+    TEST(SpaceText) {
+        CHECK_EQUAL(wst(4,L"PROGRAM MIROVANIE"),"PRIARARNOMOIGMVE");
+    }
+    TEST(EmptyText) {
+        CHECK_THROW(wst(4,L" "),cipher_error);
+    }
+    TEST(ValiDTextWithoutletters) {
+        CHECK_THROW(wst(4,L"!*><?/,.123"),cipher_error);
+    }
+    TEST(TextWithNumber) {
+        CHECK_EQUAL(wst(4,L"PRograM123miroVANie"),"PRIARARNOMOIGMVE");
+    }
+    TEST(TextWithSpaceAndPunct) {
+        CHECK_EQUAL(wst(4,L"The programmer walks!"),"TRARKHOMWSEGMA PREL ");
+    }
+}
+SUITE(DecryptText)
+{
+    TEST(ValidTEXT) {
+        CHECK_EQUAL(wst1(4,L"PRIARARNOMOIGMVE"),"PROGRAMMIROVANIE");
+    }
+    TEST(LowTEXT) {
+        CHECK_EQUAL(wst1(4,L"PriaRARNomoIGMve"),"PROGRAMMIROVANIE");
+    }
+    TEST(SpaceTEXT) {
+        CHECK_EQUAL(wst1(4,L"PRIARARN OMOIGMVE"),"PROGRAMMIROVANIE");
+    }
+    TEST(EmptyTEXT) {
+        CHECK_THROW(wst1(4,L" "),cipher_error);
+    }
+    TEST(TextNumberText) {
+        CHECK_EQUAL(wst1(4,L"PRIARARN123OMOIGMVE"),"PROGRAMMIROVANIE");
+    }
+    TEST(TextSymbolText) {
+        CHECK_EQUAL(wst1(4,L"PRIARARN!!!OMOIGMVE"),"PROGRAMMIROVANIE");
+    }
+
+}
+
+int main()
+{
+    return UnitTest::RunAllTests();
+}
